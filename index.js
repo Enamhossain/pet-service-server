321acdd15f4cfd657ae31efb5a5a8a87aa5f4610
Enamhossain <bhaianam9@gmail.com>
2022-11-23 17:09:45 +0600

    database connect get service

--- index.js
+++ index.js
@@ -10,15 +10,28 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vz4h6lc.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vz4h6lc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+  try {
+      const serviceCollection = client.db('petCare').collection('service');
+
+      app.get('/services', async (req, res) => {
+          const query = {}
+          const cursor = serviceCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+      });
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
   res.send('Hello pet service server!')
